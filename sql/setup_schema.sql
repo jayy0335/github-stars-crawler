5be27f910a53ce08ac16d78aa2f91ac48bf966d9
Jazib Ali Khan <110601284+jayy0335@users.noreply.github.com>
2025-09-17 09:07:29 +0500

    Update setup_schema.sql

--- sql/setup_schema.sql
+++ sql/setup_schema.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS repositories;
 
--- Create repositories table
 CREATE TABLE IF NOT EXISTS repositories (
     repo_id TEXT PRIMARY KEY,
     name_with_owner TEXT NOT NULL,
@@ -12,5 +11,4 @@ CREATE TABLE IF NOT EXISTS repositories (
     fetched_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
--- Index to help lookup by name
 CREATE INDEX IF NOT EXISTS idx_repositories_name_owner ON repositories (name_with_owner);
