0eb1b709629bcef945a0b8f18f7bd4201b9a17b8
Jazib Ali Khan <jazibkhan7ctech@gmail.com>
2025-11-29 02:29:27 +0500

    Update Codef

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ class Config:
     PGPASSWORD = os.getenv("PGPASSWORD", "postgres") 
     PGDATABASE = os.getenv("PGDATABASE", "postgres")
 
-    TARGET_COUNT = int(os.getenv("TARGET_COUNT", "2000"))
+    TARGET_COUNT = int(os.getenv("TARGET_COUNT", "100000"))
     DAYS_PER_SLICE = int(os.getenv("DAYS_PER_SLICE", "3"))
-    PAGE_BATCH = int(os.getenv("BATCH_SIZE", "100"))
+    PAGE_BATCH = int(os.getenv("BATCH_SIZE", "1000"))
 
     OUT_CSV = os.getenv("OUT_CSV", "artifacts/repos_stars.csv")
 
